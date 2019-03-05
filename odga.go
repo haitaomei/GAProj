e09e61a61debb04d0a7853827f474356782fd27e
Haitao <HaiTao@nas10-240-183-33.york.ac.uk>
2019-03-05 20:11:50 +0000

    api dummy

--- odga.go
+++ odga.go
@@ -23,11 +23,11 @@ var res string
 
 func main() {
 	// init db
-	// client = redis.NewClient(&redis.Options{
-	// 	Addr:     "redis-master.default.svc.cluster.local:6379",
-	// 	Password: "",
-	// 	DB:       1,
-	// })
+	client = redis.NewClient(&redis.Options{
+		Addr:     "redis-master.default.svc.cluster.local:6379",
+		Password: "",
+		DB:       1,
+	})
 
 	router := mux.NewRouter()
 
@@ -58,12 +58,11 @@ func pushHandler(httpResp http.ResponseWriter, httpReq *http.Request) {
 
 	body, _ := ioutil.ReadAll(httpReq.Body)
 	fmt.Println("Received push request, going to save into db\n\tid=", islandID, "body:", body)
-	res = string(body)
-	//save to db
-	// err := client.Set(islandID, body, 0).Err()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	// save to db
+	err := client.Set(islandID, string(body), 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func pollHandler(httpResp http.ResponseWriter, httpReq *http.Request) {
@@ -71,17 +70,14 @@ func pollHandler(httpResp http.ResponseWriter, httpReq *http.Request) {
 	islandID := vars["name"]
 	fmt.Println("Received poll request, going to save into db\n\tid=", islandID)
 	//read from db
-	// val2, err := client.Get(islandID).Result()
-	// if err == redis.Nil {
-	// 	fmt.Println("key2 does not exist")
-	// } else if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	httpResp.Header().Add("Content-Type", "application/json")
-	// 	httpResp.WriteHeader(200)
-	// 	fmt.Fprintf(httpResp, "%s", val2)
-	// }
-	httpResp.Header().Add("Content-Type", "application/json")
-	httpResp.WriteHeader(200)
-	fmt.Fprintf(httpResp, "%s", res)
+	val2, err := client.Get(islandID).Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		fmt.Println(err)
+	} else {
+		httpResp.Header().Add("Content-Type", "application/json")
+		httpResp.WriteHeader(200)
+		fmt.Fprintf(httpResp, "%s", val2)
+	}
 }
